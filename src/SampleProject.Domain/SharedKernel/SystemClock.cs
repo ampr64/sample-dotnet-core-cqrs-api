cc405320669bd8968b0c5be8c9cd8b6e97b5f9ae
Alvaro Prieto <alvaro.prieto@eversource.com>
2025-03-09 23:21:16 -0300

    Simplify SystemClock.Now

--- src/SampleProject.Domain/SharedKernel/SystemClock.cs
+++ src/SampleProject.Domain/SharedKernel/SystemClock.cs
@@ -4,18 +4,7 @@ public static class SystemClock
 {
     private static DateTime? _customDate;
 
-    public static DateTime Now
-    {
-        get
-        {
-            if (_customDate.HasValue)
-            {
-                return _customDate.Value;
-            }
-
-            return DateTime.UtcNow;
-        }
-    }
+    public static DateTime Now => _customDate.GetValueOrDefault(DateTime.UtcNow);
 
     public static void Set(DateTime customDate) => _customDate = customDate;
 
