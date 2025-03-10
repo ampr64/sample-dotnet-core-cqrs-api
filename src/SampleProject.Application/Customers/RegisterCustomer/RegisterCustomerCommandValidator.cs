974f385cb3898bcc7a855b080c5e7e67e7271f0e
Alvaro Prieto <alvaro.prieto@eversource.com>
2025-03-10 14:21:47 -0300

    Add validations for customer registration

---
+++ src/SampleProject.Application/Customers/RegisterCustomer/RegisterCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace SampleProject.Application.Customers.RegisterCustomer;
+
+public class RegisterCustomerCommandValidator : AbstractValidator<RegisterCustomerCommand>
+{
+    public RegisterCustomerCommandValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is empty");
+        RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not a valid email address");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is empty");
+    }
+}
